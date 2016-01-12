79bfdedea6ca1de4fe2519c37354f6b4d62755a2
Elizabeth Huntsman <elizabethhuntsman@Elizabeths-iMac.local>
2016-01-12 09:43:28 -0700

    redirect on authentication failure

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
+OmniAuth.config.on_failure = Proc.new { |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :identity,
             fields: [:first_name, :last_name, :email],
